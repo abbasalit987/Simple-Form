9c6866bd650359a0001782beaaf5799ae7369ee5
abbasalit987 <abbasalit987@gmail.com>
2023-02-28 20:56:02 +0530

    commit 04

--- main.js
+++ main.js
@@ -4,21 +4,17 @@ console.log('Hello World!');
 let loginForm = document.getElementById("loginForm");
 loginForm.addEventListener("submit", (e) => {
     e.preventDefault();
-  
+    
     // handle submit
     firstName = document.getElementById('firstName').value;
     lastName = document.getElementById('lastName').value;
 
     fullName = firstName + ' ' + lastName;
     console.log(fullName);
-    eMail = document.getElementById('eMail').value;
+    eMail = document.getElementById("eMail").value;
     var x = document.getElementById("subject");
     var i = x.selectedIndex;
     subject = document.getElementById("subject").innerHTML = x.options[i].text;
-
-    // localStorage.setItem('fullName',fullName);
-    // localStorage.setItem('email',eMail);
-    // localStorage.setItem('subject',subject);
 
     let user_details = {
         name: fullName,
