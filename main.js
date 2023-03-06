3aed6b1b8391dc8d991b6a70a56cfa5f9689ca4b
abbasalit987 <abbasalit987@gmail.com>
2023-03-06 12:24:08 +0530

    commit 05 - adding/deleting users

--- main.js
+++ main.js
@@ -2,19 +2,19 @@ console.log('Hello World!');
 // console.log(document.getElementsByName('field1'));
 
 let loginForm = document.getElementById("loginForm");
-loginForm.addEventListener("submit", (e) => {
-    e.preventDefault();
+function save(event) {
     
-    // handle submit
-    firstName = document.getElementById('firstName').value;
-    lastName = document.getElementById('lastName').value;
+    event.preventDefault();
+
+    firstName = event.target.field1.value;
+    lastName = event.target.field2.value;
 
     fullName = firstName + ' ' + lastName;
     console.log(fullName);
-    eMail = document.getElementById("eMail").value;
+    eMail = event.target.field3.value;
     var x = document.getElementById("subject");
     var i = x.selectedIndex;
-    subject = document.getElementById("subject").innerHTML = x.options[i].text;
+    subject = event.target.field4.value = x.options[i].text;
 
     let user_details = {
         name: fullName,
@@ -26,4 +26,27 @@ loginForm.addEventListener("submit", (e) => {
     console.log(user_details_serialized);
 
     localStorage.setItem(eMail,user_details_serialized);
-  });
+    addUser(user_details);
+  }
+
+  function addUser(user_details) {
+    
+    const parentElem = document.getElementById('users');
+    const childElem = document.createElement('li');
+    childElem.textContent = user_details.name + ' - ' + user_details.email_id + ' - ' + user_details.subject_opted;
+
+    const deleteBtn = document.createElement('input');
+    deleteBtn.type = 'button'
+    deleteBtn.value = 'Delete'
+    deleteBtn.onclick = () => {
+        localStorage.removeItem(user_details.email_id);
+        parentElem.removeChild(childElem);
+    }
+    childElem.appendChild(deleteBtn);
+    parentElem.appendChild(childElem);
+  }
+
+
+
+  
+
